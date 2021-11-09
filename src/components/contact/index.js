929350f9ff79f47614994a8ae63d24bcf67fd0b2
NguyenBv <nguyenbv@rikkeisoft.com>
2021-11-09 23:24:32 +0700

    feat: loki fix

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -10,26 +10,20 @@ const Contact = () => {
             alt="chú rể"
             src="https://lh3.googleusercontent.com/tNwp2zEtLKyJLlqjjnFIF-uZHJidR9Ne43goZssRpNu9WBTc3fAUU8iTL_CRaSeQcqd9wYMDvwZS47z2NAZs_b8QUOHj6h5sd6VX-H74k5lD7gp_nRZaYeisLJDwLAjRQSj0pbHAfQ=w2400"
           />
-          <a
-            href="https://www.facebook.com/nguyen.hero"
-            target="_blank"
-            className="btn-link"
-          >
-            <img
-              alt="socical"
-              src="https://img.icons8.com/color/48/ffffff/facebook.png"
-            />
-          </a>
-          <a
-           href="tel:0967267469"
-            target="_blank"
-            className="btn-link"
-          >
-            <img
-              alt="socical"
-              src="https://img.icons8.com/color/48/ffffff/apple-phone.png"
-            />
-          </a>
+          <div className="btn-link">
+            <a href="https://www.facebook.com/nguyen.hero" target="_blank"  rel="noreferrer" >
+              <img
+                alt="socical"
+                src="https://img.icons8.com/color/48/ffffff/facebook.png"
+              />
+            </a>
+            <a href="tel:0967267469" target="_blank"  rel="noreferrer" >
+              <img
+                alt="socical"
+                src="https://img.icons8.com/color/48/ffffff/apple-phone.png"
+              />
+            </a>
+          </div>
         </div>
         <img
           alt="loved"
@@ -41,26 +35,20 @@ const Contact = () => {
             src="https://lh3.googleusercontent.com/PS2Kx9hWsKU2W_XxEAs0qyF7cUj3e8q9_gNav8b-qVq4bhAoiDOeXFbPhshr3kgutux48gVq88_pftrEQFDYpKdzdYCqHdTGwpNoPp4NQB7K36-wv6fxGqd8q-7FF-PpJ3HKWCC9nw=w2400"
             alt="cô dâu"
           />
-          <a
-            href="https://www.facebook.com/huonglt.hus7694"
-            target="_blank"
-            className="btn-link"
-          >
-            <img
-              alt="socical"
-              src="https://img.icons8.com/color/48/ffffff/facebook.png"
-            />
-          </a>{" "}
-          <a
-           href="tel:0987442906"
-            target="_blank"
-            className="btn-link"
-          >
-            <img
-              alt="socical"
-              src="https://img.icons8.com/color/48/ffffff/apple-phone.png"
-            />
-          </a>
+          <div className="btn-link">
+            <a href="https://www.facebook.com/huonglt.hus7694" target="_blank"  rel="noreferrer" >
+              <img
+                alt="socical"
+                src="https://img.icons8.com/color/48/ffffff/facebook.png"
+              />
+            </a>{" "}
+            <a href="tel:0987442906" target="_blank"  rel="noreferrer" >
+              <img
+                alt="socical"
+                src="https://img.icons8.com/color/48/ffffff/apple-phone.png"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
